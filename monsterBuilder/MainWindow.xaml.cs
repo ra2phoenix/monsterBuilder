c2a13cd5f60c56054e73cdc0c3e1a8c1b2ce0e9e
Matthew Prior <ra2phoenix@gmail.com>
2018-11-11 17:35:52 -0800

    Initial commit

---
+++ monsterBuilder/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections.Generic;
+using System.Windows;
+
+
+namespace monsterBuilder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Action> actions = new List<Action>();
+            dgActions.ItemsSource = actions;
+
+            
+        }
+    }
+
+    public class Action
+    {
+        public string Name { get; set; }
+
+        public string Text { get; set; }
+
+        public string Type { get; set; }
+    }
+}
